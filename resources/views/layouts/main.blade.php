9a0a8b3c51367db0d4e2933477ff4908c6c598de
Melvin <kirigayakazzeno@gmail.com>
2024-12-09 15:01:11 +0700

    add css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,6 +16,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
+    <link rel="stylesheet" href="{{ asset('css/temp.css') }}">
+
     {{-- Vite dipakai untuk manual CSS & JS + Auto Reload --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
